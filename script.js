b43d708431b587b9185d046eed2283df113b1649
Juan Piedra <juan.piedra@rocketmail.com>
2023-05-09 18:44:27 -0700

    Added basics to get started

---
+++ script.js
@@ -0,0 +1,19 @@
+var APIkey = "5e1fbf26e7999bc26ec11e0fd6c6c7b1";
+var coords = document.getElementById("coords");
+
+function citySearch() {
+    var userInput = document.getElementById("userInput").value;
+    var cityAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=${APIkey}`;
+  
+    fetch(cityAPI).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+        	var lat = data[0].lat;
+        	var lon = data[0].lon;
+
+			coords.innerHTML = lat + ", " + lon;
+			
+        });
+      }
+    });
+  }
