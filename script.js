864c56f586910d308fd2074add347dbc1909ab1a
BryceedThompson <128741755+BryceedThompson@users.noreply.github.com>
2023-05-15 13:00:34 -0700

    got geosearch working needs clean up

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var APIkey = "5e1fbf26e7999bc26ec11e0fd6c6c7b1";
 var coords = document.getElementById("coords");
 var historyArr = JSON.parse(localStorage.getItem("historyArr")) || [];
 
+
 function citySearch() {
 	var userInput = document.getElementById("userInput").value;
 	if (userInput == ""){
@@ -22,7 +23,16 @@ function citySearch() {
         response.json().then(function (data) {
         	var latCoord = data[0].lat;
         	var lonCoord = data[0].lon;
-			
+
+
+			var geoCoordsSearch = latCoord + "|" + lonCoord;
+			// console.log("####" + geoCoordsSearch);
+			wikiGeo(geoCoordsSearch);
+
+			for(var i = 0; i < wikiGeoPlacesArray.length; i++) {
+				wikiName(wikiGeoPlacesArray[i]);
+			}
+
 			let center = [lonCoord, latCoord];
 			const map = tt.map({
 				key: "jYV23H6TtSrbMJGiwDcq5hEw8TVbcnQn",
@@ -49,3 +59,60 @@ function citySearch() {
 for (var i = 0; i < historyArr.length; i++) {
 	searchHistory(historyArr[i]);
 }
+
+var wikiGeoPlacesArray = [];
+var wikiInfoDisplay = document.querySelector(".wikiDislplay");
+
+function wikiGeo(geoCoordsSearch){
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "query",
+    list: "geosearch",
+    gscoord: geoCoordsSearch,
+    gsradius: "10000",
+    gslimit: "10",
+    format: "json"
+};
+console.log(params);
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {
+        var pages = response.query.geosearch;
+        for (var place in pages) {
+            //console.log(pages[place].title);
+			wikiGeoPlacesArray.push(pages[place].title);
+        }
+    })
+    .catch(function(error){console.log(error);});
+console.log(wikiGeoPlacesArray);
+}
+
+
+
+
+
+function wikiName (nameSearch){
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "opensearch",
+    search: nameSearch,
+    limit: "1",
+    namespace: "0",
+    format: "json"
+};
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {console.log(response);})
+    .catch(function(error){console.log(error);});
+}
+
